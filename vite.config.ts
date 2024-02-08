9e71d294acc45d5b217519ccd64077073af8d81c
Noemivg <ananoemi.venturagonzalez@ge.com>
2024-02-08 00:50:47 -0600

    changed base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-pokemon-app/',
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
